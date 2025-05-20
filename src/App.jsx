a57d52f873229595f68c2ec836d7e21bd275e2ea
adriancr93 <adriancr93@hotmail.com>
2025-05-20 09:45:47 -0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Guitar from './components/Guitar'
+
+function App() {
+
+
+
+  return (
+    <>
+
+    <Header/>
+
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+           <Guitar />
+        </div>
+    </main>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+    </footer>
+    
+    </>
+  )
+}
+
+export default App
+
+
+/*
+// State 
+  const [auth, setAuth] = useState(false)
+  const [total, setTotal] = useState(0)
+  const [cart, setCart] = useState([])
+*/
